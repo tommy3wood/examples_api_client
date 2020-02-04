5af1b09e09fc65a58595e6364a1f4e6ecdf18da5
tommy3wood <thomaswcurran1014@gmail.com>
2020-02-04 12:28:30 -0600

    encoded commit

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -2,7 +2,7 @@ class Api::ArticlesController < ApplicationController
   def index
     search_term = params[:search] 
     response = HTTP
-                    .headers("X-Api-Key" => "9d8d763bee474258b084bc58edff2dfd")
+                    .headers("X-Api-Key" => Rails.application.credentials.news_api[:api_key])
                     .get("https://newsapi.org/v2/everything?q=#{ search_term }")
     @articles = response.parse
     render 'index.json.jb'
